0e193b6becdc3c4261bbf168303c9ca9efe914e9
BatuhanYigit <batuhanyigit34@gmail.com>
2022-12-07 18:17:20 +0300

    pandas_denemeleri

---
+++ pandas_pratik/pandas-test123.py
@@ -0,0 +1,37 @@
+import pandas as pd
+df = pd.read_excel('/home/batuhan/Documents/GitHub/Banka-Uygulamasi-V1/output.xlsx')
+username = input("KUllanıcı adınızı giriniz : ")
+password = input("Şİfrenizi giriniz : ")
+
+data1 = {
+  "Username": [username],
+  "Password": [password]
+}
+df1 = pd.DataFrame(data1)
+
+kontrol_username = input("KUllanıcı adınızı giriniz : ")
+kontrol_password = input("Şifrenizi giriniz : ")
+
+# data2 = {
+#   "Username": [username],
+#   "Password": [password]
+# }
+# df2 = pd.DataFrame(data2)
+
+# newdf = df1.append(df2)
+
+
+
+df = df.append(df1)
+
+
+df.to_excel("output.xlsx", index=False)
+print(df["Username"][0])
+
+if(kontrol_username == df["Username"][0]):
+     print("Merhaba HOşgeldiniz")
+else:
+     print("Yanlış")
+
+
+
